c54e62eb22d9e8deecc2d8783b7bba0c89d4ab81
jasmic <50964113+jasmic@users.noreply.github.com>
2024-05-08 09:37:13 +0100

    Add files via upload

---
+++ diceGame 002.py
@@ -0,0 +1,25 @@
+#Dice game - Stage 2
+
+#throw two six sided dice
+#print the result
+
+#Declare variables and import modules
+import random
+
+Player1score = 0
+
+#Dice throw code
+throw1 = random.randint(1,6)
+throw2 = random.randint(1,6)
+print("You threw a",throw1,"and a",throw2)
+
+#code to decide scoring
+#code for a draw
+if throw1 == throw2:
+    print("You threw a double!")
+    Player1score = (throw1 + throw2) + 10
+    extraThrow = random.randint(1,6)
+    print("You threw an extra ",extraThrow)
+    Player1score = Player1score + extraThrow
+
+#need the code for two different numbers
