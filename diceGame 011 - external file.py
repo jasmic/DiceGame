a52b2425f6c989e0e695886210245f89ce7d1771
jasmic <50964113+jasmic@users.noreply.github.com>
2024-05-08 10:29:56 +0100

    Add files via upload

---
+++ diceGame 011 - external file.py
@@ -0,0 +1,41 @@
+#creating, reading and appending external files
+
+#1. Find your folder you want to save the file in
+#2. Copy the path to the folder
+#3. The text doesn't appear until you close the file
+
+write_file=open(r'#paste your path here - add the name of the file to the end of the path','w')
+write_file.write("This is the first sentence in our file.")
+write_file.close()
+
+#append
+
+append_file=open(r'#paste the path here again','a')
+append_file.write(" This is our second sentence in our file.")
+append_file.close()
+
+#context manager
+#Don't need to use the close command
+
+with open((r'#paste the path here again','a')as append_file:
+    append_file.write("\nThis is our third sentence in our file, on a new line")
+
+#read contents of file
+
+with open(r'#paste the path here again','r')as read_file:
+    print(read_file.read())
+
+#append multi lines
+
+
+multi_line = """
+This is the fourth sentence.
+This is the fifth sentence.
+And this is the sixth sentence.
+"""
+    
+with open(r'#paste the path here again','a')as append_file:
+    append_file.write(multi_line)
+
+with open(r'#paste the path here again','r')as read_file:
+    print(read_file.read())
