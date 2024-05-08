53319d5aacebf62046d389d37b90affa4b35e303
jasmic <50964113+jasmic@users.noreply.github.com>
2024-05-08 09:34:52 +0100

    Add files via upload

---
+++ diceGame 001.py
@@ -0,0 +1,18 @@
+#Dice game - Stage 1
+
+#throw two six sided dice
+#print the result
+
+#Declare variables and import modules
+import random
+
+Player1score = 0
+
+#Dice throw code
+throw1 = random.randint(1,6)
+throw2 = random.randint(1,6)
+print("You threw a",throw1,"and a",throw2)
+
+
+
+#code tested and working as expected
